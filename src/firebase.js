5892af58b1dc884b176c650875354c85fa72f56d
Rupesh8055 <rupeshbdsh@gmail.com>
2018-06-02 16:13:35 +0530

    firebase js added

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDATA2bq-F8Ke_ukPoTrR08CoezjgLhePk",
+    authDomain: "webtv-b6d6f.firebaseapp.com",
+    databaseURL: "https://webtv-b6d6f.firebaseio.com",
+    projectId: "webtv-b6d6f",
+    storageBucket: "webtv-b6d6f.appspot.com",
+    messagingSenderId: "684026666840"
+  };
+  firebase.initializeApp(config);
+</script>
